2c7b6ada9a0a3c99e6d81144be7fa206a7e52c23
Frank Rosendorf <frank.rosendorf1@gmail.com>
2019-05-05 22:44:24 -0400

    Add ApiView for the next liked dog and modified tests

--- pug_or_ugh/tests.py
+++ pug_or_ugh/tests.py
@@ -1,7 +1,9 @@
 from rest_framework import status
 from rest_framework.test import APITestCase
+from rest_framework.authtoken.models import Token
+from django.contrib.auth.models import User
 from django.urls import reverse
-from .models import Dog
+from .models import Dog, UserPreference
 
 dog = {
   "name": "Elsa",
@@ -21,20 +23,68 @@ dog2 = {
   "size": "l"
 }
 
-class DogViewTests(APITestCase):
+# class DogViewTests(APITestCase):
+
+#     def setUp(self):
+#         self.dog = Dog.objects.create(**dog)
+#         self.dog2 = Dog.objects.create(**dog2)
+
+#     def test_dog_list(self):
+#         res = self.client.get(reverse('dogs:dog_list'))
+#         self.assertEqual(res.status_code, status.HTTP_200_OK)
+#         self.assertIn(self.dog, res.data)
+#         self.assertIn(self.dog2, res.data)
+
+#     def test_dog_detail(self):
+#         res = self.client.get(reverse('dogs:dog_detail', kwargs={'pk': self.dog.id}))
+#         print(res.data)
+#         self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+
+class UserPreferenceViewTests(APITestCase):
 
     def setUp(self):
-        self.dog = Dog.objects.create(**dog)
-        self.dog2 = Dog.objects.create(**dog2)
+        self.user = User.objects.create(
+          username='testUser123',
+          email='test.user@gmail.com',
+          password='testPassword1'
+        )
+        Token.objects.create(user=self.user)
+        self.token = Token.objects.get(user__username='testUser123')
+        UserPreference.objects.create(
+          size= 'm',
+          gender= 'f',
+          age= 'b',
+          user=self.user
+        )
+        # authenticate all reqeusts
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
 
-    def test_dog_list(self):
-        res = self.client.get(reverse('dogs:dog_list'))
-        self.assertEqual(res.status_code, status.HTTP_200_OK)
-        self.assertIn(self.dog, res.data)
-        self.assertIn(self.dog2, res.data)
+    def test_duplicate_user_preferences(self):
+        data = {
+          'size': 'm',
+          'gender': 'f',
+          'age': 'b'
+        }
+        url = reverse('dogs:user_preferences')
+        res = self.client.post(url, data=data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(res['Location'], url)
+        self.assertEqual(res.data['detail'], 'This user has already created their preferences.')
 
-    def test_dog_detail(self):
-        res = self.client.get(reverse('dogs:dog_detail', kwargs={'pk': self.dog.id}))
-        print(res.data)
+    def test_get_user_preferences(self):
+        res = self.client.get(reverse('dogs:user_preferences'))
         self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(res.data), 3)
+
+    def test_update_user_preferences(self):
+        updated_data = {
+          'size': 'm',
+          'gender': 'm',
+          'age': 's'
+        }
+        url = reverse('dogs:user_preferences')
+        res = self.client.put(url, data=updated_data)
+        self.assertEqual(res.data, '')
+        self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
     
--- pug_or_ugh/views.py
+++ pug_or_ugh/views.py
@@ -4,31 +4,11 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.exceptions import NotFound
 from django.db.models import ObjectDoesNotExist
+from django.urls import reverse
+from django.db.models import Q
 from . import models
 from . import serializers
 
-# TODO Set up routes
-"""
-To get the next liked/disliked/undecided dog
-GET requests
-/api/dog/<pk>/liked/next/
-/api/dog/<pk>/disliked/next/
-/api/dog/<pk>/undecided/next/
-"""
-
-"""
-To change the dog' status, 
-PUT requests
-/api/dog/<pk>/liked/
-/api/dog/<pk>/disliked/
-/api/dog/<pk>/undecided/
-"""
-
-"""
-To change a users's preferences
-POST requests initially, then a put request
-/api/user/preferences/
-"""
 
 """
 Notes on Django Token Auth
@@ -54,11 +34,25 @@ class UserProfile(APIView):
     permission_classes = (IsAuthenticated,)
 
     def post(self, request, format=None):
-        # since UserPref has a oneToOne relationship with a user it should not allow duplicates
-        serializer = serializers.UserPrefSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save(user=request.user)
-        return Response(serializer.data, status.HTTP_201_CREATED)
+        headers = {'location': reverse('dogs:user_preferences')}
+        try:
+            # to send a nicer message to the client
+            existing_pref = models.UserPreference.objects.get(user=request.user)
+        except ObjectDoesNotExist:
+            serializer = serializers.UserPrefSerializer(data=request.data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save(user=request.user)
+            return Response(
+                data='', 
+                status=status.HTTP_201_CREATED, 
+                headers=headers
+            )
+        else:
+            return Response(
+                data={'detail': 'This user has already created their preferences.'},
+                status=status.HTTP_400_BAD_REQUEST,
+                headers=headers
+            )
 
     def get(self, request, format=None):
         """Find the users preferences"""
@@ -78,4 +72,56 @@ class UserProfile(APIView):
         serializer = serializers.UserPrefSerializer(user_preferences, data=request.data)
         serializer.is_valid()
         serializer.save()
-        return Response(serializer.data)
+        return Response(
+            data='', 
+            status=status.HTTP_204_NO_CONTENT,
+            headers={'location': reverse('dogs:user_preferences')}
+        )
+
+
+"""
+To get the next liked/disliked/undecided dog
+GET requests
+/api/dog/<pk>/liked/next/
+/api/dog/<pk>/disliked/next/
+/api/dog/<pk>/undecided/next/
+"""
+class NextLikedDogView(APIView):
+    """
+        Retrieves the next dog from the queryset of liked dogs
+        :pk: will identify the current dog
+    """
+    permission_classes=(IsAuthenticated,)
+
+    def get(self, request, pk, format=None):
+        dogs = models.Dog.objects.filter(
+            Q(userdog__user__id=request.user.id) &
+            Q(userdog__status='l') &
+            Q(id__gt=pk)
+        )
+        serializer = serializers.DogSerializer(dogs.first())
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class NextDogUnliked(APIView):
+    """
+        Retrieves the next dog from the queryset of unliked dogs
+        :pk: will identify the current dog
+    """
+    pass
+
+
+class NextDogUndecided(APIView):
+    """
+        Retrieves the next dog from the queryset of undecided dogs
+        :pk: will identify the current dog
+    """
+    pass
+
+"""
+To change the dog' status (UserDog model), 
+PUT requests
+/api/dog/<pk>/liked/
+/api/dog/<pk>/disliked/
+/api/dog/<pk>/undecided/
+"""
